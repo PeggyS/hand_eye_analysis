d79b08b62a4536b2e7394537f4c9aeca011d72a1
PeggyS <peggy.skelly@gmail.com>
2018-07-07 15:54:51 -0400

    add number of saccades to summary file

--- summarize_export_file.m
+++ summarize_export_file.m
@@ -45,6 +45,13 @@ for ex_cnt = 1:length(excl_inds)
 	end
 end
 
+out_fname = strrep(fname, '.txt', '_summary.txt');
+
+fid = fopen(out_fname, 'w');
+fprintf(fid, 'total time  = %g\n', time_in_file);
+fprintf(fid, 'excluded time = %g\n', excluded_time_total);
+
+
 % saccade summary
 sacc_type_list = {'lh' 'lv' 'rh' 'rv'};
 sacc_source_list = {'EDF_PARSER', 'findsaccs', 'engbert'};
@@ -52,23 +59,29 @@ for eye_cnt = 1:length(sacc_type_list)
 	for source_cnt = 1:length(sacc_source_list)
 		sacc_fname = strrep(fname, '.txt', ['_', sacc_source_list{source_cnt} '_' sacc_type_list{eye_cnt} '.txt']);
 		if exist(sacc_fname, 'file')
-			keyboard
+% 			keyboard
 			sacc_tbl = readtable(sacc_fname);
 			% remove exluded time segments
 			for ex_seg_cnt = 1:excluded_segments_count
-				keyboard
+% 				keyboard
 				sacc_tbl(sacc_tbl.startTime >= exclude_beg_time(ex_seg_cnt) & ...
 						 sacc_tbl.startTime <= exclude_end_time(ex_seg_cnt), :) = [];
 			end % excluded segments
+			
+			% number of < 1° saccades
+			small_sacc_inds = find(sacc_tbl.Ampl < 1);
+			fprintf(fid, '%s %s number of < 1 deg saccades = %d\n', sacc_source_list{source_cnt}, ...
+				sacc_type_list{eye_cnt}, length(small_sacc_inds));
+			
+			% number of >= 1° saccades
+			large_sacc_inds = find(sacc_tbl.Ampl >= 1);
+			fprintf(fid, '%s %s number of >= 1 deg saccades = %d\n', sacc_source_list{source_cnt}, ...
+				sacc_type_list{eye_cnt}, length(large_sacc_inds));
 		end
 	end % sacc_source
 end % sacc_type
 
-out_fname = strrep(fname, '.txt', '_summary.txt');
-
-fid = fopen(out_fname, 'w');
-fprintf(fid, 'total time  = %g\n', time_in_file);
-fprintf(fid, 'excluded time = %g\n', excluded_time_total);
+fclose(fid);
 
 
 
