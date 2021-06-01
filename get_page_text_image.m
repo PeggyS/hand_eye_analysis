fdae2c392dd10e332294986fab05123d7e9ce7e6
PeggyS <peggy.skelly@gmail.com>
2021-06-01 17:59:53 -0400

    add try-catch around finding reading page image

--- get_page_text_image.m
+++ get_page_text_image.m
@@ -7,7 +7,16 @@ underscore_num = strrep(tmp{1}, '.bin', '');
 num = strrep(underscore_num, '_', '');
 % find od, os, or ou in path
 tmp = regexpi(handles.bin_filename, '(od)|(os)|(ou)/', 'match');
-eye = strrep(lower(tmp{1}), '/', '');
+try
+   eye = strrep(lower(tmp{1}), '/', '');
+catch
+   beep;pause(0.33);beep;pause(0.33);beep
+   fprintf('I could not determine which eye to use.\n');
+   fprintf('It should be included in the name of the data file.\n');
+   fprintf('Type "dbquit" to exit debugger.\n');
+   keyboard
+   return
+end
 
 page_fname = [eye num '.jpg'];
 [pathstr, ~, ~] = fileparts(handles.bin_filename);
@@ -16,9 +25,16 @@ page_fname = [eye num '.jpg'];
 % get the text page image 
 if exist(fullfile(pathstr, page_fname),'file') % if it's in the same folder as the data
 	handles.img_file = fullfile(pathstr, page_fname);
-	handles.im_data = imread(handles.img_file);
+   try
+      handles.im_data = imread(handles.img_file);
+   catch
+   fprintf('I could not read the image file.\n');
+   fprintf('Type "dbquit" to exit debugger.\n');
+   keyboard
+   return
+   end
+   
 else
-
 	% go up 2 folder levels and look for the file
 	[pathname,filename] = findfilepath(page_fname, '../..');
 	if isempty(pathname)
